172ef3ce2596d8004265c52250e0b2545eb96516
eldk24 <165836989+eldk24@users.noreply.github.com>
2024-05-17 03:06:46 -0500

    Chatbot inicio

---
+++ Proyecto/public/js/app2.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const output = document.getElementById('output');
+    const input = document.getElementById('input');
+
+    function showMessage(message) {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.innerText = message;
+        output.appendChild(messageElement);
+    }
+
+    function handleUserInput(userInput) {
+        if (userInput === 'Plataforma') {
+            showMessage('¿Qué plataforma estás buscando? Opciones: Nintendo, PlayStation, Xbox, PC');
+        } else if (userInput === 'Accesorios') {
+            window.location.href = 'accesorios.html';
+        } else if (['Nintendo', 'PlayStation', 'Xbox', 'PC'].includes(userInput)) {
+            // Verifica si el userInput está en la lista de opciones de plataforma
+            switch (userInput) {
+                case 'Nintendo':
+                    window.location.href = 'nintendopag.html';
+                    break;
+                case 'PlayStation':
+                    window.location.href = 'pspag.html';
+                    break;
+                case 'Xbox':
+                    window.location.href = 'xboxpag.html';
+                    break;
+                case 'PC':
+                    window.location.href = 'pcpag.html';
+                    break;
+                default:
+                    showMessage('Lo siento, no entiendo esa opción.');
+            }
+        } else {
+            showMessage('Lo siento, no entiendo esa opción.');
+        }
+    }
+
+    input.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            const userInput = input.value.trim();
+            if (userInput !== '') {
+                showMessage(`Usuario: ${userInput}`);
+                handleUserInput(userInput);
+                input.value = '';
+            }
+        }
+    });
+
+    showMessage('¿Qué categoría estás buscando? Opciones: Plataforma, Accesorios');
+});

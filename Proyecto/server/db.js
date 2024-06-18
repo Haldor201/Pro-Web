7336cd32ee1449ca207d41039bb5db7038c377f4
Haldor201 <calderonhaldor385@gmail.com>
2024-06-18 12:17:28 -0500

    base de datos en la nube

--- Proyecto/server/db.js
+++ Proyecto/server/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conectar=async ()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/TallerWeb');   
+        await mongoose.connect('mongodb+srv://calderonhaldor385:Vp9Szac8E3nvDEWZ@so.de31nf8.mongodb.net/');   
         console.log("Conectado");
     } catch (error) {
         console.log(error);

7c22d80c1d3d1e1c59a6b20fef3cfb2bb653ccbc
Haldor201 <calderonhaldor385@gmail.com>
2024-06-23 09:16:28 -0500

    rutas protegidas

--- Proyecto/Games/src/App.jsx
+++ Proyecto/Games/src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 import { Routes,Route,BrowserRouter as Router } from "react-router-dom";
 import Login from './components/Login';
 import Home from './components/Home';
+import ProfilePage from './components/ProfilePage.jsx';
 import Register from './components/Register';
+import ProtectedRoutes from "./ProtectedRoutes.jsx";
 function App() {
   return (
     <Router>
@@ -10,6 +12,10 @@ function App() {
         <Route path="/Login" element={<Login/>}></Route>
         <Route path="/Home" element={<Home/>}></Route>
         <Route path="/Register" element={<Register/>}></Route>
+
+        <Route element={<ProtectedRoutes></ProtectedRoutes>}>
+          <Route path="/profile" element={<ProfilePage></ProfilePage>} />
+        </Route>
       </Routes>
     </Router>
   )
---
+++ Proyecto/Games/src/ProtectedRoutes.jsx
@@ -0,0 +1,12 @@
+import { useAuth } from "./context/authcontext";
+import { Navigate, Outlet } from "react-router-dom";
+export default function ProtectedRoutes() {
+  const { loading,isAuthenticated } = useAuth();
+
+  if(loading) return <h1>Loading</h1> 
+  if (!loading&&!isAuthenticated) {
+    return <Navigate to="/login" replace></Navigate>;
+  }
+  /**si esta autenticado, que siga con el componente que esta dentro */
+  return <Outlet></Outlet>;
+}

75ffe9e295e55af8b29dd7882b166f1afdb93aca
Haldor Eliezer <101613536+Haldor201@users.noreply.github.com>
2024-08-20 08:00:24 -0500

    Update db.js

--- Proyecto/server/db.js
+++ Proyecto/server/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conectar=async ()=>{
     try {
-        await mongoose.connect('mongodb+srv://calderonhaldor385:Vp9Szac8E3nvDEWZ@so.de31nf8.mongodb.net/');   
+        await mongoose.connect('');   
         console.log("Conectado");
     } catch (error) {
         console.log(error);
